4eec2b1aae5f9d5a1e0dd44860dad3c2a092fb78
DreadPirate <duckmanjbr@gmail.com>
2018-06-02 13:39:42 -0500

    Update pfSense_local-repo.sh

--- pfSense_local-repo.sh
+++ pfSense_local-repo.sh
@@ -2,22 +2,22 @@
 #
 #Info
 #=======================
-#       File: pfSense_local-repo.sh
-#       Name: Create local repositories on FreeBSD (pfSense)
+#	File: pfSense_local-repo.sh
+#	Name: Create local repositories on FreeBSD (pfSense)
 #
-        VERSION_NUM='1.0'
-#       *Version is major.minor format
-#       *Major is updated when new capability is added
-#       *Minor is updated on fixes and improvements
+	VERSION_NUM='1.0'
+# 	*Version is major.minor format
+# 	*Major is updated when new capability is added
+# 	*Minor is updated on fixes and improvements
 #
 #History
-#=======================
-#       13Dec2017 v1.0
-#               Dread Pirate
-#               *Initial build to create local repo and pull a list of packages.
+#=======================		
+#	13Dec2017 v1.0 
+#		Dread Pirate
+#		*Initial build to create local repo and pull a list of packages.
+#	
 #
-#
-#Description
+#Description 
 #=======================
 # This script creates a local repository on FreeBSD and pulls down the added packages into the new repo.
 # This script will also disable all remote repositories and stop pfSense from calling out to pfSense.org.
@@ -31,9 +31,17 @@
 #
 REPO='offline'
 REPO_CONF='local_repo.conf'
-PACKAGES='pfSense-pkg-Backup pfSense-pkg-Cron pfSense-pkg-Notes pfSense-pkg-Open-VM-Tools pfSense-pkg-Shellcmd pfSense-pkg-S
-tatus_Traffic_Totals pfSense-pkg-bandwidthd pfSense-pkg-openvpn-client-export pfSense-pkg-tftpd'
-
+PACKAGES=""
+PACKAGES="${PACKAGES} pfSense-pkg-Backup"
+PACKAGES="${PACKAGES} pfSense-pkg-Cron"
+PACKAGES="${PACKAGES} pfSense-pkg-Notes"
+PACKAGES="${PACKAGES} pfSense-pkg-Open-VM-Tools"
+PACKAGES="${PACKAGES} pfSense-pkg-Shellcmd"
+PACKAGES="${PACKAGES} pfSense-pkg-Status_Traffic_Totals"
+PACKAGES="${PACKAGES} pfSense-pkg-bandwidthd"
+PACKAGES="${PACKAGES} pfSense-pkg-openvpn-client-export"
+PACKAGES="${PACKAGES} pfSense-pkg-tftpd"
+#
 #####################################################
 #Script Variables
 #
@@ -41,79 +49,15 @@ REPO_DIR='/usr/local/etc/pkg/repos'
 #####################################################
 #Functions:
 #
-Header ()
-{
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-}
-#
-Footer ()  
-{
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-}
-# 
-Download_packages ()
-#
-REPO_DIR='/usr/local/etc/pkg/repos'
-#####################################################
-#Functions:
-#
-Header ()
-{
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-}
-#
 Download_packages ()
 {
 #Download all the packages into the new repository directory.
-Header
-echo "+++++++++++   Updating packages   +++++++++++++++++++"
-Header
-echo ""
 pkg upgrade
 pkg update
 mkdir -p "$REPO_DIR"/"$REPO"
-Header
-echo "+++++++++++   Fetching packages   +++++++++++++++++++"
-Header
-echo ""
-pkg fetch -y -d -o "$REPO_DIR"/"$REPO" "$PACKAGES"
-# https://github.com/Duckmanjbr/DIP_Config/blob/master/pfSense_local-repo.sh
-#
-#####################################################
-#Selectable Variables (user selected)
-#
-REPO='offline'
-REPO_CONF='local_repo.conf'
-PACKAGES='pfSense-pkg-Backup pfSense-pkg-Cron pfSense-pkg-Notes pfSense-pkg-Open-VM-Tools pfSense-pkg-Shellcmd pfSense-pkg-S
-tatus_Traffic_Totals pfSense-pkg-bandwidthd pfSense-pkg-openvpn-client-export pfSense-pkg-tftpd'
-
-#####################################################
-#Script Variables
-#
-REPO_DIR='/usr/local/etc/pkg/repos'
-#####################################################
-#Functions:
-#
-Header ()
-{
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-}
-#
-Download_packages ()
-{
-#Download all the packages into the new repository directory.
-Header
-echo "+++++++++++   Updating packages   +++++++++++++++++++"
-Header
-echo ""
-pkg upgrade
-pkg update
-mkdir -p "$REPO_DIR"/"$REPO"
-Header
-echo "+++++++++++   Fetching packages   +++++++++++++++++++"
-Header
-echo ""
-pkg fetch -y -d -o "$REPO_DIR"/"$REPO" "$PACKAGES"
+for PACKAGE in $PACKAGES; do
+        pkg fetch -y -d -o "$REPO_DIR"/"$REPO" $PACKAGE
+done
 }
 #
 #===================================
